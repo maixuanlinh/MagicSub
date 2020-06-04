c085c52c6fd5fabd861632e50a8b44e195fea495
Mai Xuan Linh <maixuanlinhdkh@gmail.com>
2020-06-05 00:38:06 +0300

    hehe. now dont need to rebuild apk. we can add link directly and process from url

--- app/src/main/java/com/maixuanlinh/magicsub/MainActivity.java
+++ app/src/main/java/com/maixuanlinh/magicsub/MainActivity.java
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.media.PlaybackParams;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,79 +28,135 @@ public class MainActivity extends AppCompatActivity {
     private Button subBtn;
     private Button cancelLastbtn;
     private Button pauseBtn;
+    private float speed = 1f;
+    private Button speedbtn;
     private ArrayList<Integer> subTimeArrayList = new ArrayList<>();
+    private EditText editText;
+    private Button getAudio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        mediaPlayer = MediaPlayer.create(MainActivity.this,R.raw.workingmother);
+
         playbtn = findViewById(R.id.playbtn);
         resultTxv = findViewById(R.id.textResult);
         subBtn = findViewById(R.id.subbtn);
         cancelLastbtn = findViewById(R.id.cancelLastbtn);
         pauseBtn = findViewById(R.id.pausebtn);
-        playbtn.setOnClickListener(new View.OnClickListener() {
+        speedbtn = findViewById(R.id.speed);
+        getAudio = findViewById(R.id.getaudiobtn);
+        editText = findViewById(R.id.editText);
+
+
+
+        getAudio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                try {
+                    String url = editText.getText().toString();
+                    Log.i("Text",url);
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setDataSource(url);
+                    mediaPlayer.prepareAsync();
 
-                mediaPlayer.start();
-            }
-        });
-
-        subBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               try {
-                   int currentPositionInSec = mediaPlayer.getCurrentPosition()/1000;
-                   subTimeArrayList.add(currentPositionInSec);
-                   String currentArrayList = subTimeArrayList.toString();
-                   resultTxv.setText(currentArrayList);
+                    mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(final MediaPlayer mp) {
+                            Toast.makeText(getApplicationContext(),"Ready",Toast.LENGTH_SHORT).show();
 
 
-               } catch (Exception e) {
+                            playbtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
 
-               }
-            }
-        });
+                                    mp.start();
+                                }
+                            });
 
-        resultTxv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                for (int i = 0; i < subTimeArrayList.size()-1;i++) {
-                    textResult += subTimeArrayList.get(i)+",";
+                            subBtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    try {
+                                        int currentPositionInSec = mp.getCurrentPosition()/1000;
+                                        subTimeArrayList.add(currentPositionInSec);
+                                        String currentArrayList = subTimeArrayList.toString();
+                                        resultTxv.setText(currentArrayList);
+
+
+                                    } catch (Exception e) {
+
+                                    }
+                                }
+                            });
+
+                            resultTxv.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    for (int i = 0; i < subTimeArrayList.size();i++) {
+                                        textResult += subTimeArrayList.get(i)+",";
+                                    }
+                                    textResult+= mp.getDuration()/1000;
+                                    ClipData clipData = ClipData.newPlainText("Result", textResult);
+                                    clipboardManager.setPrimaryClip(clipData);
+                                }
+                            });
+
+                            cancelLastbtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    subTimeArrayList.remove(subTimeArrayList.size()-1);
+                                    mp.seekTo(subTimeArrayList.get(subTimeArrayList.size()-1)*1000);
+                                    if (!mp.isPlaying()) { mp.start();}
+                                    String currentArrayList = subTimeArrayList.toString();
+                                    resultTxv.setText(currentArrayList);
+                                }
+                            });
+
+                            pauseBtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    mp.pause();
+                                }
+                            });
+
+                            pauseBtn.setOnLongClickListener(new View.OnLongClickListener() {
+                                @Override
+                                public boolean onLongClick(View view) {
+                                    mp.seekTo(0);
+
+                                    return false;
+                                }
+                            });
+
+                            speedbtn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    if (speed == 2f) {
+                                        speed = 1f;
+
+                                    } else {
+                                        speed += 0.5f;
+                                    }
+                                    mp.setPlaybackParams(new PlaybackParams().setSpeed(speed));
+                                }
+                            });
+
+                        }
+                    });
+
+
+
+
+
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.i("IO Exception", "caught "+e.getMessage());
                 }
-                textResult+= subTimeArrayList.get(subTimeArrayList.size()-1);
-                ClipData clipData = ClipData.newPlainText("Result", textResult);
-                clipboardManager.setPrimaryClip(clipData);
             }
         });
-
-        cancelLastbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                    subTimeArrayList.remove(subTimeArrayList.size()-1);
-                    mediaPlayer.seekTo(subTimeArrayList.get(subTimeArrayList.size()-1)*1000);
-                    if (!mediaPlayer.isPlaying()) { mediaPlayer.start();}
-            }
-        });
-
-        pauseBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mediaPlayer.pause();
-            }
-        });
-
-        pauseBtn.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                mediaPlayer.seekTo(0);
-
-                return false;
-            }
-        });
-
 
 
 
